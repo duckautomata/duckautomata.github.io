42c9e025ff85ee1d4a4f82e1964244c52594b116
duckautomata <duckmechatomata@gmail.com>
2025-09-26 23:40:17 -0500

    Turning off maintenance

--- live-transcript/config.js
+++ live-transcript/config.js
@@ -1,3 +1,3 @@
-window.maintenance=true;
-window.maintenanceText1="The server that processes the audio transcriptions is refusing to boot 🤷.";
-window.maintenanceText2="This means the site will be down until I can either fix the boot issue or replace the hardware. Who knows how long this will take.";
+window.maintenance=false;
+window.maintenanceText1="";
+window.maintenanceText2="";
