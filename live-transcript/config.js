2da3b24e28b9185b329d6c41ef74b89aa6d6a243
duckautomata <duckmechatomata@gmail.com>
2026-01-23 22:36:52 -0600

    Enable maintenance mode

--- live-transcript/config.js
+++ live-transcript/config.js
@@ -1,3 +1,3 @@
-window.maintenance=false;
-window.maintenanceText1="";
-window.maintenanceText2="";
+window.maintenance=true;
+window.maintenanceText1="I'm an idiot and accidentally deleted the server.";
+window.maintenanceText2="The site will be back up hopefully in 24 hours.";
